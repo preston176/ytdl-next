dc9a5e1aae331a4d3380e7c4b6a4103007781e51
preston176 <prestonnyamweya@gmail.com>
2024-08-16 11:37:33 +0300

    chore: Add Navbar component and update layout for better navigation

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,68 @@
+"use client"
+import { useState, useEffect } from 'react';
+import { Bars3BottomRightIcon, XMarkIcon } from '@heroicons/react/24/solid';
+import Link from 'next/link';
+
+const Navbar = () => {
+    const links = [
+        { name: 'Home', link: '/' },
+        { name: 'About me', link: 'about' },
+        { name: 'Projects', link: 'projects' },
+        { name: 'Contact me', link: 'contact' },
+    ];
+
+    const [open, setOpen] = useState<boolean>(false);
+
+    useEffect(() => {
+        setOpen(false); // Close the mobile menu when the component mounts
+    }, []);
+
+    const toggleMenu = () => {
+        setOpen(!open);
+    };
+
+    return (
+        <div className="w-full max-w-screen fixed top-0 left-0 z-50">
+            <div className="md:flex items-center justify-center py-4 md:px-10 px-7">
+  
+
+                {/* md: mobile icons */}
+                <div
+                    onClick={toggleMenu}
+                    className="w-7 h-7 right-8 top-6 cursor-pointer md:hidden text-black absolute"
+                >
+                    {open ? <XMarkIcon /> : <Bars3BottomRightIcon />}
+                </div>
+
+                {/* navbar */}
+                <ul
+                    className={`sticky md:flex md:items-center md:pd-0 md:static md:z-auto z-10 left-0 w-full md:w-auto mt-4 md:pl-0 pl-9 sm:bg-transparent transition-all duration-500 ease-in-out mobile-menu ${open ? 'open' : ''}`}
+                >
+                    {links.map((item, index) => (
+                        <li key={index} className="md:ml-8 md:my-0 my:7 font-semibold">
+                            {item.link.startsWith('http') ? (
+                                <a
+                                    href={item.link}
+                                    className="text-black transition-all duration-500 hover:text-primary"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    {item.name}
+                                </a>
+                            ) : (
+                                <Link
+                                    href={item.link}
+                                    className="text-black transition-all duration-500 hover:text-primary"
+                                >
+                                    {item.name}
+                                </Link>
+                            )}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
